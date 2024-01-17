284d39cf750ec51a5ff7cdcffb7ba6ef299c59cd
lmsand <lmsand99@icloud.com>
2024-01-16 21:51:46 -0600

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by lexi sanders on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
